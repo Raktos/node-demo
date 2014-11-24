cbcd12efc9c019e21589c2e03dc7ab4d64983669
Information School <ischoolit@uw.edu>
2014-11-24 10:58:12 -0800

    hello world server

--- hello-server.js
+++ hello-server.js
@@ -6,3 +6,13 @@
 
 'use strict';
 
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World!\n');
+});
+
+server.listen(8080, function() {
+    console.log('Server is listening at http://localhost:8080');
+});
